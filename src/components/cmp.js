d7bb72301b9126db078e006377369ada13ed0dcb
pompeu <itacir@hotmail.com>
2016-06-30 11:23:37 -0300

    create testing for componet with a service

---
+++ src/components/cmp.js
@@ -0,0 +1,21 @@
+
+function AppCtrl (Posts) {
+  const vm = this;
+
+  vm.ok = () => true;
+
+  vm.init = () => {
+    return Posts.list()
+      .then(data => vm.allPosts = data);
+  }
+}
+
+AppCtrl.$inject = ['Posts']
+
+module.exports = angular.module('app.dir', ['app.posts'])
+  .component('appCmp', {
+    restrict : 'E',
+    template : '<md-button> enter </md-button>',
+    controller: AppCtrl,
+    controllerAs : 'vm' 
+  });
---
+++ src/components/cmp.spec.js
@@ -0,0 +1,56 @@
+import cmp from './cmp'
+import Posts from '../services/posts'
+
+describe('Cmp', () => {
+  let $compile, $rootScope, vm, $el, $controller, posts, $q
+
+  beforeEach(() => {
+    angular.mock.module(cmp.name)
+    angular.mock.module(Posts.name)
+  })
+
+  beforeEach(angular.mock.inject((_$rootScope_, _$compile_, _Posts_, _$q_) => {
+    $compile   = _$compile_
+    $rootScope = _$rootScope_
+    $q         = _$q_
+    posts      = _Posts_
+    $el        = angular.element('<app-cmp></app-cmp>')
+    $compile($el)(_$rootScope_.$new())
+    _$rootScope_.$digest()
+    $controller = $el.isolateScope()
+    vm = $controller.vm
+  }))
+
+  describe('Cmp testing', () => {
+    it('should be ok function exits', () => {
+      expect(vm.ok).toBeDefined()
+    })
+
+    it('should be ok return true', () => {
+      expect(vm.ok()).toBe(true)
+    })
+
+    it('should be method init exist', () => {
+      expect(vm.init).toBeDefined()
+    })
+
+    it('should be Posts.list called when init() execult', () => {
+      spyOn(posts, 'list').and.callThrough()
+      vm.init()
+      expect(posts.list).toHaveBeenCalled()
+    })
+
+    it('should be vm.init reponse with promise', done => {
+      const deferred = $q.defer()
+      deferred.resolve('ok')
+      spyOn(posts, 'list').and.returnValue(deferred.promise)
+      vm.init()
+        .then(data => { 
+          expect(data).toEqual('ok')
+          expect(vm.allPosts).toEqual('ok')
+          done()
+        })
+      $rootScope.$digest()
+    })
+  })
+})
